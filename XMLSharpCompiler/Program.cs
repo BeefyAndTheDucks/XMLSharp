725663a0a8f66068e80b29fde37ed60cebded92f
Simon Lotzfeldt <72296886+BeefyAndTheDucks@users.noreply.github.com>
2026-05-02 21:22:25 +0200

    Initial Commit

---
+++ XMLSharpCompiler/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace XMLSharpCompiler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Goodbye sanity =)");
+    }
+}
---
+++ XMLSharpCompilerTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace XMLSharpCompilerTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Fail("incompetent porgammer lnguage alert");
+    }
+}
