babbb5d768837d2b7f660cec3e157fafd986b001
Simon Lotzfeldt <72296886+BeefyAndTheDucks@users.noreply.github.com>
2026-05-02 23:16:10 +0200

    Basic program loop

--- XMLSharpCompiler/Program.cs
+++ XMLSharpCompiler/Program.cs
@@ -1,13 +1,20 @@
-﻿namespace XMLSharpCompiler;
+﻿using System.Diagnostics;
+
+namespace XMLSharpCompiler;
 
 class Program
 {
     static void Main(string[] args)
     {
-        ILexer lexer = new Lexer();
-
-        Token[] tokens = lexer.Lex("");
+        Stopwatch sw = Stopwatch.StartNew();
         
-        Console.WriteLine("Goodbye sanity =)");
+        ILexer lexer = new Lexer();
+        IAstGenerator astGenerator = new AstGenerator();
+
+        Token[] tokens = lexer.Lex("number foo = 2;");
+        
+        AstNode ast = astGenerator.Generate(tokens);
+        
+        Console.WriteLine($"Compilation completed in {sw.ElapsedMilliseconds}ms. Result: {ast}");
     }
 }
