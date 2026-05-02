d789cf16e4dadd8ab8354158f0c078da1af3aa5b
Meelee35 <124620845+Meelee35@users.noreply.github.com>
2026-05-02 20:44:22 +0100

    add token types

--- XMLSharpCompiler/Lexer.cs
+++ XMLSharpCompiler/Lexer.cs
@@ -1,11 +1,34 @@
 namespace XMLSharpCompiler;
 
+// tokens
+public abstract class Token { }
+
+public class IdentifierToken(string name) : Token
+{
+    public string Name = name;
+}
+
+public class ImmediateToken(int value) : Token
+{
+    public int Value = value;
+}
+
+public class AssignmentToken : Token { }
+public class AddToken : Token { }
+public class SemicolonToken : Token { }
+
+// actual code
 public class Lexer : ILexer
 {
+    private static readonly TokenDefinition[] Definitions = {
+
+    };
+
     public Token[] Lex(string input)
     {
         List<Token> tokens = [];
-        
-        // parsing here
+
+        int i = 0;
+
     }
 }
