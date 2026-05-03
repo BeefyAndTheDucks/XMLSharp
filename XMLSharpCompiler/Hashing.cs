3b88f3dc4d882436806dbba096ecd1213cc57502
Meelee35 <124620845+Meelee35@users.noreply.github.com>
2026-05-03 11:26:21 +0100

    Update tamper protection to something not broken

--- XMLSharpCompiler/Hashing.cs
+++ XMLSharpCompiler/Hashing.cs
@@ -1,4 +1,8 @@
+using System;
+using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace XMLSharpCompiler;
 
@@ -6,21 +10,35 @@ public static class Hashing
 {
     public static void TamperProtection()
     {
-        byte[] expected = Convert.FromHexString("0f5366b3b19afc3184d23bc73d8cd311");
-        byte[] actual = Hash();
-
-        if (!expected.SequenceEqual(actual))
+        try
         {
-            throw new Exception("Unknown error.");
+            byte[] expected = Convert.FromHexString("a95dc4e899ac156e6e0b6666dd970c9959e0e4fd948177c263543b23492768ec");
+            byte[] actual = Hash();
+
+            if (!expected.SequenceEqual(actual))
+            {
+                Fail();
+            }
+        }
+        catch
+        {
+            Fail();
         }
     }
 
     private static byte[] Hash()
     {
         byte[] p = [46, 47, 105, 109, 112, 111, 114, 116, 97, 110, 116, 47, 120, 97, 118, 105, 101, 114, 46, 116, 120, 116];
-        string f = System.Text.Encoding.ASCII.GetString(p);
-        using var h = MD5.Create();
+        string f = Encoding.ASCII.GetString(p);
+
+        using var h = SHA256.Create();
         using var s = File.OpenRead(f);
         return h.ComputeHash(s);
+    }
+
+    private static void Fail()
+    {
+        Console.WriteLine("Unknown error.");
+        Environment.Exit(1);
     }
 }
